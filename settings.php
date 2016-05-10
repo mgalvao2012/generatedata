46640305cd80cc4f35107694c0f34f86a998db73
mgalvao2012 <mgalvao2012@gmail.com>
2016-05-10 18:59:14 -0300

    updated with real configurations

--- settings.php
+++ settings.php
@@ -5,10 +5,10 @@
  * in most cases automatically create this file, but worst-case scenario (no permissions, for example) it will
  * at least generate the file content so you can create it yourself.
  */
-$dbHostname     = '';
-$dbName         = '';
-$dbUsername     = '';
-$dbPassword     = '';
-$dbTablePrefix  = '';
+$dbHostname     = 'us-cdbr-iron-east-04.cleardb.net';
+$dbName         = 'heroku_e3b9bcbbb1baaf3';
+$dbUsername     = 'b24b0c2a665185';
+$dbPassword     = 'c61ad97e';
+$dbTablePrefix  = 'gd_';
 $encryptionSalt = 'ABC'; // any 3 A-Z chars
 $apiEnabled     = false;
